434f4852bf9e216772655f3b5c2a18f8894b4975
sergvoronich <85866901+sergvoronich@users.noreply.github.com>
2021-10-02 18:01:22 +0300

    Update script4.js

--- script4.js
+++ script4.js
@@ -41,13 +41,51 @@ function HashStorageFunction() {
 
 // вариант с классом
 
-// не получилось!
+// не получилось...
 
+// а потом получилось!
 
+let storage = {};  // не мог поместить никак объект-хранилище внутри класса, пришлось вот так поступить.
 
+class HashStorageFunction2 {
 
+    constructor(storage) {
+        this.storage = storage;
+    }
 
+    addValue = function (key, value) {
+        this.key = key;
+        this.value = value;
+        storage[key] = value;
+    }
 
+    getValue = function (key) {
+        this.key = key;
+        if (key == "undefined") {
+            return false;
+        } else {
+            return storage[key];
+        }
+    }
 
+    deleteValue = function (key) {
+        this.key = key;
+        if (storage[key]) {
+            delete storage[key];
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    getKeys = function () {
+        var ar = [];
+        for (let item in storage) {
+            ar.push(item);
+        }
+        return ar;
+    }
+
+}
 
 
