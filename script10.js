e9cae291ab26e0ef03d7d728af0d67567660f51e
sergvoronich <85866901+sergvoronich@users.noreply.github.com>
2021-10-23 15:10:36 +0300

    Add files via upload

---
+++ script10.js
@@ -0,0 +1,27 @@
+
+const container = document.querySelector(".content-container");
+container.addEventListener('click', deploy);
+
+function deploy(e) {
+    const image = e.target;
+    if (image.classList.contains("small-picture")) {
+        let ar = document.getElementsByClassName("big-picture");
+        if (ar.length < 4) {
+            const bigs = document.querySelector(".big-pictures");
+            const newbig = document.querySelector(".big-picture:last-child");
+            newbig.src = image.src;
+            const copy = newbig.cloneNode(true);
+            newbig.classList.add("move");
+            bigs.appendChild(copy);
+        } else {
+            const bigs = document.querySelector(".big-pictures");
+            const newbig = document.querySelector(".big-picture:last-child");
+            newbig.src = image.src;
+            const copy = newbig.cloneNode(true);
+            newbig.classList.add("move");
+            bigs.appendChild(copy);
+            const oldbig = document.querySelector(".big-picture:nth-child(2)");
+            bigs.removeChild(oldbig);
+        }
+    }
+}
