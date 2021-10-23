d0381a98a074b8baec62d5d6a25c51fadbad708b
sergvoronich <85866901+sergvoronich@users.noreply.github.com>
2021-10-23 13:09:31 +0300

    Update script10.js

--- script10.js
+++ script10.js
@@ -1,29 +1,10 @@
-
 const container = document.querySelector(".content-container");
+const bigimage = document.querySelector(".big-picture");
 container.addEventListener('click', deploy);
 
 function deploy(e) {
     const image = e.target;
     if (image.classList.contains("small-picture")) {
-        let ar = document.getElementsByClassName("big-picture");
-        if (ar.length < 4) {
-            const bigs = document.querySelector(".big-pictures");
-            const newbig = document.querySelector(".big-picture:last-child");
-            newbig.src = image.src;
-            newbig.classList.add("move");
-            const copy = newbig.cloneNode(true);
-            copy.classList.remove("move");
-            bigs.appendChild(copy);
-        } else {
-            const bigs = document.querySelector(".big-pictures");
-            const newbig = document.querySelector(".big-picture:last-child");
-            newbig.src = image.src;
-            newbig.classList.add("move");
-            const copy = newbig.cloneNode(true);
-            copy.classList.remove("move");
-            bigs.appendChild(copy);
-            const oldbig = document.querySelector(".big-picture:nth-child(2)");
-            bigs.removeChild(oldbig);
-        }
+        bigimage.src = image.src;
     }
 }
