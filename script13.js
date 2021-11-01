ff19a263a5d5d03d47e1692c1ee467dee62aad00
sergvoronich <85866901+sergvoronich@users.noreply.github.com>
2021-11-01 12:47:59 +0300

    Add files via upload

---
+++ script13.js
@@ -0,0 +1,40 @@
+
+let dot1 = document.getElementById("dot1");
+let horiz = 0;
+let vert = 0;
+let timer;
+let speed = 5;
+
+function moveDot() {
+    if (horiz <= (568) && vert == 0) {
+        horiz += speed;
+        dot1.style.left = horiz + "px";
+        timer = requestAnimationFrame(moveDot);
+    } else if (vert <= (208) && horiz > 30) {
+        vert += speed;
+        dot1.style.top = vert + "px";
+        timer = requestAnimationFrame(moveDot);
+    } else if (horiz >= 2) {
+        horiz -= speed;
+        dot1.style.left = horiz + "px";
+        timer = requestAnimationFrame(moveDot);
+    } else if (vert >= 2) {
+        vert -= speed;
+        dot1.style.top = vert + "px";
+        timer = requestAnimationFrame(moveDot);
+    }
+}
+
+const button = document.querySelector(".button");
+const button2 = document.querySelector(".button2");
+button.addEventListener('click', startMove);
+button2.addEventListener('click', stopMove);
+
+function startMove() {
+    timer = requestAnimationFrame(moveDot);
+}
+
+function stopMove() {
+    cancelAnimationFrame(timer);
+}
+
